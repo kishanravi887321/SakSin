778deaa5a76016815bc02f1c5ad693b2829b22e9
Ravi Kishan <kishanravi887321@gmail.com>
2025-07-06 00:11:21 +0530

    configuration of app completed

--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+# import  dj_database_url
+from dotenv  import load_dotenv 
+load_dotenv()
+import  os 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -38,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'apps.accounts',
+    
     'apps.interviews',
     'apps.subscriptions',
     'apps.analytics',
