b42c9c25faf7684ac68b8fa948ab573310a0c8a5
Ravi Kishan <kishanravi887321@gmail.com>
2025-07-11 23:23:51 +0530

    chanegs

--- apps/accounts/api/urls.py
+++ apps/accounts/api/urls.py
@@ -4,7 +4,7 @@ from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns=[
     path('register/',RegisterView.as_view(),name='reg-view'),
-    path('login/',CustomTokenObtainPairView.as_view(),name='login-view'),
+    # path('login/',CustomTokenObtainPairView.as_view(),name='login-view'),
     path('update-password/', UpdatePasswordView.as_view(), name='update-password-view'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token-refresh-view'),
     path('update-profile/', ProfileImageUploadView.as_view(), name='profile-image-upload-view')
