65ea1615dcbcc62a59f0273dcadfa3cfbdb51429
Ravi Kishan <kishanravi887321@gmail.com>
2025-07-17 17:14:00 +0530

    feat: Update ALLOWED_HOSTS to include 'saksin.online' for production environment

--- src/config/settings.py
+++ src/config/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.getenv('SECRET')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'saksin.onrender.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'saksin.onrender.com','saksin.online']
 
 CORS_ALLOW_ALL_ORIGINS = True
 
