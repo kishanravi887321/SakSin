c77288689c8d1052ac04a20703629933c303dee9
Ravi Kishan <kishanravi887321@gmail.com>
2025-08-22 23:14:40 +0530

    fix: Clean up ALLOWED_HOSTS and CORS_ALLOWED_ORIGINS for production settings

--- src/config/settings.py
+++ src/config/settings.py
@@ -32,20 +32,13 @@ SECRET_KEY = os.getenv('SECRET')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 ALLOWED_HOSTS = [
-    'localhost',
-    '127.0.0.1',
-    'saksin.vercel.app',
-    'saksin.online',
+  
     'www.saksin.online',
-    'sakin.onrender.com'
+    
 ]
 CORS_ALLOWED_ORIGINS = [
     "https://saksin.online",
-    "https://www.saksin.online",
-    "https://saksin.vercel.app",  # ✅ Add your backend domain
-    "https://saksin-ui-nextjs.onrender.com",
-    "http://localhost:3000",
-    "http://127.0.0.1:3000"
+    
 ]
 
 # ALLOWED_HOSTS = [
