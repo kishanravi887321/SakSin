2665ae5a8cc0b8bbfc7403aa52b596a7befa12e2
Ravi Kishan <kishanravi887321@gmail.com>
2025-08-22 22:10:14 +0530

    feat: Update ALLOWED_HOSTS to include production domains

--- src/config/settings.py
+++ src/config/settings.py
@@ -48,8 +48,14 @@ DEBUG = False
 #     "http://127.0.0.1:3000"
 # ]
 
-
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    'saksin.vercel.app',
+    'saksin.online',
+    'www.saksin.online',
+    'sakin.onrender.com'
+]
 # CORS_ALLOWED_ORIGINS = [
 #     "http://127.0.0.1:5500",  
 #     "http://localhost:5500",
